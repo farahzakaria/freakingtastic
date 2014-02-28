5f13ea1115421eca67ab3c80924321b263de30ed
Faizal Zakaria <phaibusiness@gmail.com>
2014-02-28 18:00:14 +0800

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vinti::Application.config.secret_token = 'cdbdef001067fb4407268a829518691b014ab68dc9e6727a3ff9543f09b5ed1b993bb02bec0ac4c6c155ec73ee126e35428e9036544dfaa4509e14e2aef7e4cd'
