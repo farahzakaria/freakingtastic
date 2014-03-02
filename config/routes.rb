5d072d2d6aa36934959bdcf96f34a9c9acf6eff2
Farah Nabilah <farahnabilah98@hotmail.com>
2014-03-02 15:30:29 +0800

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Vinti::Application.routes.draw do
-  root to: "landing_pages#rules"
+  root to: "landing_pages#index"
 
   get '/coming_soon', :to => "landing_pages#coming_soon", as: :coming_soon
 end
